64a295ea9527fe33f9c2a42e10559061c9f7d7b3
LuongVu <lgvu307@gmail.com~>
2024-08-09 22:43:47 +0700

    ver1

--- NB.py
+++ NB.py
@@ -2,7 +2,7 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.model_selection import train_test_split 
 import pandas as pd
 
-def train(data_path, X_cols, y_cols, type, sample_weight, alpha):
+def train(data, X_cols, y_cols, type, sample_weight, alpha):
     """
         Summary of function.
 
@@ -12,15 +12,15 @@ def train(data_path, X_cols, y_cols, type, sample_weight, alpha):
 
         Parameters (json file):
         ------------------------
-        data_path : string
-            The file path to a csv fil containing the data
-        X_cols: list
+        data : csv
+            A csv fil containing the data
+        X_cols: list of string
             List of the features of the data
-        y_cols: list
+        y_cols: list of string
             List of the targets of the data
         type: string
             Type of Naive Bayes model
-        sample_weight: list
+        sample_weight: list of float
             A list of weights for each features 
         alpha: float/ int
             A number to make training more stable
@@ -32,7 +32,7 @@ def train(data_path, X_cols, y_cols, type, sample_weight, alpha):
             a model that can predict other unknown value
     """
 
-    data = pd.read_csv(data_path)
+    data = pd.DataFrame(data)
 
     if type.lower() == "gaussian naive bayes":
         model = GaussianNB(var_smoothing=alpha)
