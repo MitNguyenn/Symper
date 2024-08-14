3dfd13132e6b392f2df32c90ba19cf7d2b74763c
Luong Vu <132726554+LuongVu307@users.noreply.github.com>
2024-08-14 23:14:39 +0700

    version 2.1.0

--- models/predict.py
+++ models/predict.py
@@ -2,23 +2,23 @@ from sklearn.externals import joblib
 
 def predict(X, model_id):
     """
-        Summary of function.
-
+        Summary of function
+    
         Description:
         -----------------------
-        Return the prediction
-
+        Returns predictions based on the provided model and input data.
+    
         Parameters:
         ------------------------
         X: pd.DataFrame
-            A DataFrame containing the X data needed to be predicted
-        model_id
-            The model_id to get the model
-
+            A DataFrame containing the input data for prediction.
+        model_id: string
+            The identifier of the model to be used for making predictions.
+    
         Returns:
         -------------------------
         y: pd.DataFrame
-            The prediction of the model
+            A DataFrame containing the predictions made by the model.
     """
 
     model = joblib.load(f"save/{model_id}")
